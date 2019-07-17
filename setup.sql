23cc2827f1d9d0b4db1bdfa76f2bd5edff745900
ben <bholsinger@gmail.com>
2019-07-17 14:47:39 -0600

    setting up folders and database

---
+++ setup.sql
@@ -0,0 +1,55 @@
+USE bcwbricks;
+
+-- CREATE TABLE bricks
+-- (
+-- id INT AUTO_INCREMENT,
+-- shape VARCHAR(255) NOT NULL,
+-- PRIMARY KEY (id)
+
+-- );
+
+-- CREATE TABLE Bsets
+-- (
+-- id INT AUTO_INCREMENT,
+-- title VARCHAR(255) NOT NULL,
+-- descr VARCHAR(255) NOT NULL,
+-- PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE setbricks 
+-- (
+-- id INT AUTO_INCREMENT,
+-- brickId INT NOT NULL,
+-- bsetId INT NOT NULL,
+
+-- INDEX (bsetsId),
+
+-- FOREIGN KEY (bsetId)
+--     REFERENCES bsets(id)
+--     ON DELETE CASCADE,
+-- FOREIGN KEY (brickId)
+--   REFERENCES bricks(id)
+--   ON DELETE CASCADE,
+
+-- PRIMARY KEY(id)
+
+
+
+-- );
+
+
+ 
+
+-- SET
+-- id 
+-- title
+-- DESCRIPTION
+
+-- Brick
+-- id 
+-- shape
+
+-- setbricks
+-- ID
+-- setid
+-- brickid
